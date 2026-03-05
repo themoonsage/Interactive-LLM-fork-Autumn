a466f613599c2e05e8320004ca9dfcc63b95957e
Warsut <kaylin367@gmail.com>
2026-03-05 13:08:57 -0500

    Bob executable made and added.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/Users/kaylinwilley/Documents/Assorted School/Senior Design/Bob/Interactive-LLM-Dashboard-/venv'
+set -gx VIRTUAL_ENV '/Users/kaylinwilley/Documents/Assorted School/Senior Design/InteractiveLLMDashboard/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
